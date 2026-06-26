ef726c83e8fab43fbd70570723559d33807d96c7
Nixon <n.kami@alustudent.com>
2026-06-26 03:34:59 -0700

    Add tasks 2, 3, 4, and 5

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    count = len(argv)
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[0]))
+    else:
+        print("{} arguments:".format(count))
+        for i, arg in enumerate(argv, start=1):
+            print("{}: {}".format(i, arg))
