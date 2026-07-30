55288c80f4767a26e3830f087ae213367e7f05f0
K-Nixon-0 <n.kami@alustudent.com>
2026-07-30 11:46:31 -0700

    Complete Python Input/Output project

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines Student class"""
+
+
+class Student:
+    """Represent a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation, filtered by attrs if given"""
+        if type(attrs) is list and all(type(a) is str for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
